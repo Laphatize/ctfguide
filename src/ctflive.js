1e511d538a12fd205128acfe5201dd643b1cb7eb
Laphatize <laphatize@protonmail.com>
2021-03-10 14:05:06 -0500

    fix id creation bug for ctflive

--- src/ctflive.js
+++ src/ctflive.js
@@ -11,7 +11,16 @@ let ejs = require('ejs');
 var urlencodedParser = bodyParser.urlencoded({
     extended: false
 })
-  
+
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 ////////////////////////////////////////////////////////
 
 
