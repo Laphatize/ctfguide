4ac4ecefe8160696f2b19ebdfa902f59986e867c
Laphatize <laphatize@protonmail.com>
2021-04-24 19:38:44 -0400

    Tidied up codebase

--- public/js/checker.js
+++ public/js/checker.js
@@ -1,33 +1,33 @@
 var userid = ""
 var username = "";
 var db = firebase.firestore();
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         userid = user.uid;
-       
-var docRef = db.collection("users").doc(userid);
 
-docRef.get().then(function(doc) {
-    if (doc.exists) {
-        if (!doc.data().username) {
-          document.getElementById("setup").style.display = "block";
-        } else {
-          username = doc.data().username
-        }
-    } 
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});
+        var docRef = db.collection("users").doc(userid);
 
-        
+        docRef.get().then(function (doc) {
+            if (doc.exists) {
+                if (!doc.data().username) {
+                    document.getElementById("setup").style.display = "block";
+                } else {
+                    username = doc.data().username
+                }
+            }
+        }).catch(function (error) {
+            console.log("Error getting document:", error);
+        });
+
+
 
         var docRef = db.collection("users").doc(user.uid);
 
-        docRef.get().then(function(doc) {
-            if (doc.data().challenges.includes(  window.location.href.split("/")[4] )) {
-            document.getElementById("creator").style.display = "block";
-            document.getElementById("adminlink").href = 
-            `./${window.location.href.split("/")[4]}/edit`
+        docRef.get().then(function (doc) {
+            if (doc.data().challenges.includes(window.location.href.split("/")[4])) {
+                document.getElementById("creator").style.display = "block";
+                document.getElementById("adminlink").href =
+                    `./${window.location.href.split("/")[4]}/edit`
             }
         });
 
@@ -46,22 +46,67 @@ docRef.get().then(function(doc) {
 document.getElementById("cd").innerHTML = document.getElementById("cd").innerHTML.replace(/\\n|\\r\\n|\\n\\r|\\r/g, '<br>');
 document.getElementById("check").addEventListener("click", submit)
 document.getElementById("cancel").addEventListener("click", hide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById("submitreport").addEventListener("click", submitr)
 
 
 function statusUpdate() {
-var request = new XMLHttpRequest();
-request.open("POST", "../../api/lastviewed")
-request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-console.log(userid)
-request.send(`uid=${userid}&id=${window.location.href.split("/")[4]}`);
+    var request = new XMLHttpRequest();
+    request.open("POST", "../../api/lastviewed")
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+    console.log(userid)
+    request.send(`uid=${userid}&id=${window.location.href.split("/")[4]}`);
 
-request.onreadystatechange = function () {
-  if (request.readyState == 4 && request.status == 200) {
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
 
-        console.log("synced with server")
-  }
-}
+            console.log("synced with server")
+        }
+    }
 }
 
 
@@ -71,13 +116,13 @@ request.onreadystatechange = function () {
 
 function submitr() {
     var request = new XMLHttpRequest();
-  request.open("POST", "https://discord.com/api/webhooks/773796502408790016/KNNR7RReVEa-o-WtHLVy1sidsl6M1bZtWBXwVRBqS9pbRoRegoDTJbJBGcYTYUPLOSfr");
+    request.open("POST", "https://discord.com/api/webhooks/773796502408790016/KNNR7RReVEa-o-WtHLVy1sidsl6M1bZtWBXwVRBqS9pbRoRegoDTJbJBGcYTYUPLOSfr");
 
     request.setRequestHeader('Content-type', 'application/json');
 
     var params = {
         username: "New Report: " + userid,
-        content: "Reason for Report: " +  document.getElementById("rreason").value + "\nChallenge ID: `" + window.location.href.split("/")[4] + "`\n" + window.location.href
+        content: "Reason for Report: " + document.getElementById("rreason").value + "\nChallenge ID: `" + window.location.href.split("/")[4] + "`\n" + window.location.href
     }
 
     request.send(JSON.stringify(params));
@@ -99,23 +144,23 @@ function report() {
 }
 
 
-function submit () {
+function submit() {
 
 
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // window.alert(xhttp.response)
-        if (xhttp.response == "good") {
-            return document.getElementById("goodjob").style.display = "block";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // window.alert(xhttp.response)
+            if (xhttp.response == "good") {
+                return document.getElementById("goodjob").style.display = "block";
+            } else {
+                document.getElementById("notification").style.display = "block";
+            }
         } else {
-            document.getElementById("notification").style.display = "block";
+            //document.getElementById("notification").style.display = "block";
         }
-    } else {
-        //document.getElementById("notification").style.display = "block";
-    }
-};
-xhttp.open("GET", `../api/checksolution?username=${username}&id=${window.location.href.split("/")[4]}&solution=${document.getElementById("solution").value}&uid=${userid}`, true);
-xhttp.send();
+    };
+    xhttp.open("GET", `../api/checksolution?username=${username}&id=${window.location.href.split("/")[4]}&solution=${document.getElementById("solution").value}&uid=${userid}`, true);
+    xhttp.send();
 }
--- src/views.js
+++ src/views.js
@@ -10,216 +10,252 @@ const { response } = require('express');
 ////////////////////////////////////////////////////////
 
 
+/*
+================
+  STATIC PAGES
+================
+  */
 
+
+// Home Page
 router.get("/", (request, response) => {
-    response.sendFile(__dirname + "/views/index.html");
+  response.status(200).sendFile(__dirname + "/views/static/site/index.html");
+});
+
+// Register Page
+router.get("/register", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/authentication/register.html");
+});
+
+// Settings Page
+router.get("/settings", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/site/settings.html");
+});
+
+// Login Page
+router.get("/login", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/authentication/login.html");
+});
+
+// Password Reset Page
+router.get("/password-reset", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/authentication/password-reset.html");
+});
+
+// Challenges Page
+router.get("/challenges", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/site/challenges.html");
+});
+
+// Privacy Page
+router.get("/privacy", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/legal/privacy.html");
+});
+
+// Terms of Service Page
+router.get("/tos", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/static/legal/tos.html");
+});
+
+
+/*
+======================
+  INTEGRATION PAGES:
+======================
+*/
+
+// Page for client to authenticate with custom auth token.
+router.get("/con", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/authentication/connecting.html");
+});
+
+
+// Confirmation page for linking STiBaRC to CTFGuide
+router.get("/link-account", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/authentication/link-account.html");
+});
+
+
+// Admin Page (Still being worked on)
+router.get("/admin", (request, response) => {
+  response.status(200).sendFile(__dirname + "/views/admin/admin.html");
+});
+
+
+// STiBaRC OAuth Callback
+// This will grab the username using the session key provided in the URL parameters and store it to the database.
+router.get("/auth/stibarc", (request, response2) => {
+
+  var sessid = request.query.sess;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+
+      db.collection('users').where('stibarc_username', '==', xhttp.responseText.split("\n")[0]).get().then(function (querySnapshot) {
+
+
+        querySnapshot.forEach(function (doc) {
+          console.log(doc.id, '=>', doc.data());
+
+          // Create a token that would have otherwise been created logging in normally.
+          admin
+            .auth()
+            .createCustomToken(doc.id)
+            .then((customToken) => {
+              // Send user to connection page with token and ID for client to authenticate with.
+              response2.redirect("/con?ct=" + customToken + "&uid=" + doc.id);
+            })
+            .catch((error) => {
+              console.log('Error creating custom token:', error);
+            });
+
+        });
+
+      });
+
+
+    }
+  };
+  xhttp.open("GET", `https://api.stibarc.com/getusername.sjs?sess=${sessid}`, true);
+  xhttp.send()
+
 });
 
 
 
-router.get("/con", (request, response) => {
-    response.sendFile(__dirname + "/views/connecting.html");
-  });
-  
-  
-  
-  router.get("/link-account", (request, response) => {
-    response.sendFile(__dirname + "/views/link-account.html");
-  });
-  
+/*
+===========================
+  CHALLENGE RELATED PAGES
+===========================
+*/
 
-  router.get("/admin", (request, response) => {
-    response.sendFile(__dirname + "/views/admin.html");
-  });
-  
-  
-  router.get("/auth/stibarc", (request, response2) => {
-  
-  
-  
-    var sessid = request.query.sess;
-  
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-
-        db.collection('users').where('stibarc_username', '==', xhttp.responseText.split("\n")[0]).get().then(function (querySnapshot) {
-  
-  
-          querySnapshot.forEach(function (doc) {
-            console.log(doc.id, '=>', doc.data());
-            admin
-              .auth()
-              .createCustomToken(doc.id)
-              .then((customToken) => {
-              
-              response2.redirect("/con?ct=" + customToken + "&uid=" + doc.id);
-            //  return;
-      //      response2.send(customToken)
-        //    console.log(customToken)
-              })
-              .catch((error) => {
-                console.log('Error creating custom token:', error);
-              });
-  
-          });
-  
-  
-        });
-  
-  
-  
-  
-  
-  
-      }
-    };
-    xhttp.open("GET", `https://api.stibarc.com/getusername.sjs?sess=${sessid}`, true);
-    xhttp.send()
-  
-  });
-  
-  
-  
-  
-  router.get("/settings", (request, response) => {
-    response.sendFile(__dirname + "/views/settings.html");
-  });
-  router.get("/login", (request, response) => {
-    response.sendFile(__dirname + "/views/login.html");
-  });
-  
-  router.get("/password-reset", (request, response) => {
-    response.sendFile(__dirname + "/views/password-reset.html");
-  });
-  router.get("/challenges", (request, response) => {
-    response.sendFile(__dirname + "/views/challenges.html");
-  });
-  
-  router.get("/privacy", (request, response) => {
-    response.sendFile(__dirname + "/views/privacy.html");
-  });
-  
-  router.get("/tos", (request, response) => {
-    response.sendFile(__dirname + "/views/tos.html");
-  });
-
-  
-
+// Challenge Page
+// Grabs ID from request, asks database for data associate with the challenge, and lastly renders it on webpage.
+// This is rendered server-side for performance reasons.
 router.get("/challenges/:id", async (request, response) => {
   console.log("hit")
-    const res = await db.collection('challenges').doc(request.params.id).update({
-      views: admin.firestore.FieldValue.increment(1),
-    });
-  
-    const docRef3 = db.collection('challenges').doc(request.params.id);
-    const doc3 = await docRef3.get();
-  
-    if (!doc3.data()) {
-      response.send("Challenge does not exist or it may have been removed.");
-    }
-  
-    if (doc3.data().challenge_author) {
-      author = doc3.data().challenge_author
-    } else {
-      author = "Unknown"
-    }
-  
-    var data = {
-      "gold": "-",
-      "bronze": "-",
-      "silver": "-"
-    }
-    var category = "";
-    if (doc3.data().leaderboards_gold) data.gold = doc3.data().leaderboards_gold;
-    if (doc3.data().leaderboards_bronze) data.bronze = doc3.data().leaderboards_bronze;
-    if (doc3.data().leaderboards_silver) data.silver = doc3.data().leaderboards_silver;
-  
-    return response.render(__dirname + "/views/challenge-template.ejs", {
-      CHALLENGE_NAME: doc3.data().title,
-      CHALLENGE_DESCRIPTION: doc3.data().problem,
-      CHALLENGE_AUTHOR: author,
-      GOLD: data.gold,
-      SILVER: data.silver,
-      BRONZE: data.bronze
-    });
-  
-    //eturn;
+  const res = await db.collection('challenges').doc(request.params.id).update({
+    views: admin.firestore.FieldValue.increment(1),
   });
-  
-  
-  
-  router.get("/challenges/:id/edit", async (request, response) => {
-  
-    const docRef3 = db.collection('challenges').doc(request.params.id);
-    const doc3 = await docRef3.get();
-  
-    if (!doc3.data()) {
-      response.send("Challenge does not exist or it may have been removed.");
-    }
-  
-    if (doc3.data().challenge_author) {
-      author = doc3.data().challenge_author
-    } else {
-      author = "Unknown"
-    }
-  
-    var data = {
-      "gold": "-",
-      "bronze": "-",
-      "silver": "-"
-    }
-  
-    if (doc3.data().leaderboards_gold) data.gold = doc3.data().leaderboards_gold;
-    if (doc3.data().leaderboards_bronze) data.bronze = doc3.data().leaderboards_bronze;
-    if (doc3.data().leaderboards_silver) data.silver = doc3.data().leaderboards_silver;
-  
-    if (!doc3.data().category) {
-      category = " none "
-    } else {
-      category = doc3.data().category;
-    }
-    var v = doc3.data().views;
-    var g = doc3.data().gattempts;
-    var a = doc3.data().attempts;
-    if (!v) v = 0;
-    if (!g) g = 0;
-    if (!a) a = 0;
-  
-    response.render(__dirname + "/views/edit-challenge.ejs", {
-      CHALLENGE_NAME: doc3.data().title,
-      CHALLENGE_DESCRIPTION: doc3.data().problem,
-      CHALLENGE_AUTHOR: author,
-      CHALLENGE_DIFFICULTY: doc3.data().difficulty.toLowerCase(),
-      GOLD: data.gold,
-      SILVER: data.silver,
-      BRONZE: data.bronze,
-      CHALLENGE_ID: request.params.id,
-      CHALLENGE_CATEGORY: category,
-      VIEWS: v,
-      GATTEMPTS: g,
-      ATTEMPTS: a
-    });
-  });
-  
-  
-  
-  
-  
-  router.get("/register", (request, response) => {
-    response.sendFile(__dirname + "/views/register.html");
-  });
-  
-  router.get("/game", (request, response) => {
-    response.sendFile(__dirname + "/views/game.html");
-  });
-  
-  router.get("/dashboard", (request, response) => {
-   
-    response.render(__dirname + "/views/dashboard.ejs", {
 
-    });
+  const docRef3 = db.collection('challenges').doc(request.params.id);
+  const doc3 = await docRef3.get();
+
+  if (!doc3.data()) {
+    response.status(400).send("Challenge does not exist or it may have been removed.");
+  }
+
+  // All new challenges will have a challenge_author defined. This purely exists for legacy challenges.
+  if (doc3.data().challenge_author) {
+    author = doc3.data().challenge_author
+  } else {
+    author = "Unknown"
+  }
+
+  // What I'm doing here may seem stupid at first, 
+  // but when directly referencing doc3.data(), if a certain rank like leaderboards_silver does not exist it causes a crash.
+
+  var data = {
+    "gold": "-",
+    "bronze": "-",
+    "silver": "-"
+  }
+  var category = "";
+  if (doc3.data().leaderboards_gold) data.gold = doc3.data().leaderboards_gold;
+  if (doc3.data().leaderboards_bronze) data.bronze = doc3.data().leaderboards_bronze;
+  if (doc3.data().leaderboards_silver) data.silver = doc3.data().leaderboards_silver;
+
+  return response.status(200).render(__dirname + "/views/dynamic/site/challenge-template.ejs", {
+    CHALLENGE_NAME: doc3.data().title,
+    CHALLENGE_DESCRIPTION: doc3.data().problem,
+    CHALLENGE_AUTHOR: author,
+    GOLD: data.gold,
+    SILVER: data.silver,
+    BRONZE: data.bronze
   });
+
+
+});
+
+
+// Edit Challenge Page
+// Some data rendered serverside for performance reasons.
+router.get("/challenges/:id/edit", async (request, response) => {
+
+  const docRef3 = db.collection('challenges').doc(request.params.id);
+  const doc3 = await docRef3.get();
+
+  if (!doc3.data()) {
+    response.status(400).send("Challenge does not exist or it may have been removed.");
+  }
+
+  if (doc3.data().challenge_author) {
+    author = doc3.data().challenge_author
+  } else {
+    author = "Unknown"
+  }
+
+  var data = {
+    "gold": "-",
+    "bronze": "-",
+    "silver": "-"
+  }
+
+  if (doc3.data().leaderboards_gold) data.gold = doc3.data().leaderboards_gold;
+  if (doc3.data().leaderboards_bronze) data.bronze = doc3.data().leaderboards_bronze;
+  if (doc3.data().leaderboards_silver) data.silver = doc3.data().leaderboards_silver;
+
+  if (!doc3.data().category) {
+    category = " none "
+  } else {
+    category = doc3.data().category;
+  }
+  var v = doc3.data().views;
+  var g = doc3.data().gattempts;
+  var a = doc3.data().attempts;
+  if (!v) v = 0;
+  if (!g) g = 0;
+  if (!a) a = 0;
+
+  response.status(200).render(__dirname + "/views/dynamic/site/edit-challenge.ejs", {
+    CHALLENGE_NAME: doc3.data().title,
+    CHALLENGE_DESCRIPTION: doc3.data().problem,
+    CHALLENGE_AUTHOR: author,
+    CHALLENGE_DIFFICULTY: doc3.data().difficulty.toLowerCase(),
+    GOLD: data.gold,
+    SILVER: data.silver,
+    BRONZE: data.bronze,
+    CHALLENGE_ID: request.params.id,
+    CHALLENGE_CATEGORY: category,
+    VIEWS: v,
+    GATTEMPTS: g,
+    ATTEMPTS: a
+  });
+});
+
+
+
+
+/*
+
+DEPRECATED: This was used for an early version of CTFLive. 
+
+router.get("/game", (request, response) => {
+  response.status(200).status(200).sendFile(__dirname + "/views/game.html");
+});
+
+*/
+
+
+
+// Dashboard Page
+router.get("/dashboard", (request, response) => {
+
+  response.status(200).status(200).render(__dirname + "/views/dynamic/site/dashboard.ejs", {
+
+  });
+});
 
 ////////////////////////////////////////////////////////
 module.exports = router;
